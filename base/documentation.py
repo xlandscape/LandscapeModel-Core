c0262137117044bde607e82adc1faf24d62d0f03
sascha <sascha.bub@xlandscape.org>
2023-07-26 13:00:20 +0200

    Normalized whitespace for creating dicumentations

--- base/documentation.py
+++ base/documentation.py
@@ -8,7 +8,6 @@ import textwrap
 import types
 import typing
 
-
 # CHANGELOG
 base.VERSION.added("1.4.9", "`base.documentation` ")
 base.VERSION.added("1.5.0", "`base.documentation` methods for documenting components")
@@ -46,8 +45,6 @@ base.VERSION.changed("1.12.5", "Enhanced contribution documentation in `base.doc
 base.VERSION.changed("1.12.6", "Refactored documentation of class members into own function in `base.documentation` ")
 base.VERSION.fixed("1.13.0", "Spelling error in `base.documentation` ")
 base.VERSION.changed("1.13.1", "Text wrapping of member documentation in `base.documentation` ")
-base.VERSION.added("1.14.1", "Links to external resources in scenario documentation generator")
-base.VERSION.added("1.14.2", "Covered time span to scenario readme generation")
 
 
 def write_changelog(name: str, version_history: base.VersionCollection, file_path: str) -> None:
@@ -63,22 +60,22 @@ def write_changelog(name: str, version_history: base.VersionCollection, file_pat
         Nothing.
     """
     with open(file_path, "w", encoding="utf-8") as f:
-        f.write("# Changelog\n")
-        f.write(f"This is the changelog for the {name}. It was automatically created on {datetime.date.today()}.")
+        f.write("# Changelog\n\n")
+        f.write(f"This is the changelog for the {name}. It was automatically created on {datetime.date.today()}.\n")
         for version in version_history:
             if version.date is None:
-                f.write(f"\n\n## [{version}]\n\n")
+                f.write(f"\n## [{version}]\n\n")
             else:
-                f.write(f"\n\n## [{version}] - {version.date}\n\n")
+                f.write(f"\n## [{version}] - {version.date}\n\n")
             f.write("### Added\n")
             for message in version.additions:
-                f.write(f"- {message}\n")
+                f.write(f"\n- {message}\n")
             f.write("\n### Changed\n")
             for message in version.changes:
-                f.write(f"- {message}\n")
+                f.write(f"\n- {message}\n")
             f.write("\n### Fixed\n")
             for message in version.fixes:
-                f.write(f"- {message}\n")
+                f.write(f"\n- {message}\n")
 
 
 def document_components(components_module: types.ModuleType, file_path: str) -> None:
@@ -174,56 +171,62 @@ def document_component(
     )
     with open(file_path, "w", encoding="utf-8") as f:
         f.write(f"""## Table of Contents
+
 * [About the project](#about-the-project)
-  * [Built With](#built-with)
+    * [Built With](#built-with)
 * [Getting Started](#getting-started)
-  * [Prerequisites](#prerequisites)
-  * [Installation](#installation)
+    * [Prerequisites](#prerequisites)
+    * [Installation](#installation)
 * [Usage](#usage)
-  * [Inputs](#inputs)
-  * [Outputs](#outputs)
+    * [Inputs](#inputs)
+    * [Outputs](#outputs)
 * [Roadmap](#roadmap)
 * [Contributing](#contributing)
 * [License](#license)
 * [Contact](#contact)
 * [Acknowledgements](#acknowledgements)
 
-
 ## About the project
+
 {inspect.cleandoc(component.__doc__)}  
 This is an automatically generated documentation based on the available code and in-line documentation. The current
-version of this document is from {datetime.date.today()}.  
+version of this document is from {datetime.date.today()}.
 
 ### Built with
+
 * Landscape Model core version {base.VERSION.latest}
 * {component.module.name} version {component.module.version} {module_doc}
 
-
 ## Getting Started
+
 The component can be used in any Landscape Model based on core version {base.VERSION.latest} or newer. See the Landscape
 Model core's `README` for general tips on how to add a component to a Landscape Model.
 
 ### Prerequisites
-A model developer that wants to add the `{component.name}` component to a Landscape Model needs to set up the general 
+
+A model developer that wants to add the `{component.name}` component to a Landscape Model needs to set up the general
 structure for a Landscape Model first. See the Landscape Model core's `README` for details on how to do so.
 
 ### Installation
-1. Copy the `{component.name}` component into the `model\\variant` sub-folder.
-2. Make use of the component by including it into the model composition using `module={component.__module__}` and 
-   `class={type(component).__name__}`. 
 
+1. Copy the `{component.name}` component into the `model\\variant` sub-folder.
+2. Make use of the component by including it into the model composition using `module={component.__module__}` and
+   `class={type(component).__name__}`.
 
 ## Usage
-The following gives a sample configuration of the `{component.name}` component. See [inputs](#inputs) and 
+
+The following gives a sample configuration of the `{component.name}` component. See [inputs](#inputs) and
 [outputs](#outputs) for further details on the component's interface.
+
 ```xml
 {sample_configuration}
 ```
 
 ### Inputs
+
 """)
         for component_input in component.inputs:
-            f.write(f"#### {component_input.name}\n")
+            f.write(f"#### {component_input.name}\n\n")
             if component_input.description:
                 f.write("\n".join(textwrap.wrap(inspect.cleandoc(component_input.description), 120)))
                 f.write("\n")
@@ -282,32 +285,32 @@ The following gives a sample configuration of the `{component.name}` component. 
                     f.write(f"The default value of the output is `{value}`.\n")
                 else:
                     raise ValueError(f"Unsupported default attribute: {attribute}")
-        f.write("\n\n## Roadmap\n")
+        f.write("\n## Roadmap\n\n")
         if len(component.VERSION.roadmap) == 0:
             f.write(f"The `{component.name}` component is stable. No further development takes place at the moment.\n")
         else:
-            f.write(f"The following changes will be part of future `{component.name}` versions:\n")
+            f.write(f"The following changes will be part of future `{component.name}` versions:\n\n")
             for item in component.VERSION.roadmap:
                 clean_doc = inspect.cleandoc(f"* {item}")
                 f.write(f"{clean_doc}\n")
         f.write("""
-
 ## Contributing
-Contributions are welcome. Please contact the authors (see [Contact](#contact)). Also consult the `CONTRIBUTING` 
+
+Contributions are welcome. Please contact the authors (see [Contact](#contact)). Also consult the `CONTRIBUTING`
 document for more information.
 
-
 ## License
+
 Distributed under the CC0 License. See `LICENSE` for more information.
 
-
 ## Contact
+
 """)
         for author in component.VERSION.authors:
-            f.write(f"{author}  \n")
-        f.write("\n\n## Acknowledgements\n")
+            f.write(f"{author}\n")
+        f.write("\n## Acknowledgements\n\n")
         for acknowledgement in component.VERSION.acknowledgements:
-            f.write(f"* {acknowledgement}  \n")
+            f.write(f"* {acknowledgement}\n")
 
 
 def write_contribution_notes(file_path: str) -> None:
@@ -322,18 +325,21 @@ def write_contribution_notes(file_path: str) -> None:
     """
     with open(file_path, "w", encoding="utf-8") as f:
         f.write(f"""# Contributing
-Contributions to the project are welcome. Please contact the authors. These contribution notes refer to the general 
+
+Contributions to the project are welcome. Please contact the authors. These contribution notes refer to the general
 Landscape Model contribution guidelines and were written on {datetime.date.today()}.
 
 ## Issues
+
 Whenever you identify a bug or like to suggest an enhancement to the code, please file an issue. Stick to the following
 checklist when submitting an issue.
+
 1. Check whether a similar issue already exists. If so, please comment on the existing issue instead of creating a new
    one.
 2. Identify the most appropriate repository for the issue, that is the repository nearest to the presumed code changes.
    For instance, if you found a bug in the base classes, please submit it in the Landscape Model core repository or an
    enhancement of a specific component in the component's repository. If you are not sure about the most appropriate
-   repository, please use the top level repository of the according model variant, e.g., xAquaticRisk or 
+   repository, please use the top level repository of the according model variant, e.g., xAquaticRisk or
    xOffFieldSoilRisk.
 3. Give the issue a strong, self-explanatory title.
 4. Provide any information in the issue's description that is needed to understand the purpose of the issue and allows
@@ -347,27 +353,29 @@ checklist when submitting an issue.
 7. In the case of a `bug` issue, you may additionally assign the `highPriority` label if the bug is breaking the normal
    usage of the application.
 8. Whenever you start working on an issue, you should assign the label `Work in progress` to it to communicate that the
-   issue is actively addressed. Likewise, if you finish work on an issue, the label `work in progress` should be 
+   issue is actively addressed. Likewise, if you finish work on an issue, the label `work in progress` should be
    removed before closing the issue.
 9. If an issue requires additional input or is delayed for future work actions, you can mark it with the label
    `Waiting`.
-10. In case another issue is blocking the work on an issue or if two issues are otherwise related, please actively link 
+10. In case another issue is blocking the work on an issue or if two issues are otherwise related, please actively link
     the issues using the according GitLab options.
 
 ## Merge requests
-The Landscape Model repositories adapt the GitFlow approach for versioning (see 
-[A successful Git branching model](https://nvie.com/posts/a-successful-git-branching-model/) for a detailed 
+
+The Landscape Model repositories adapt the GitFlow approach for versioning (see
+[A successful Git branching model](https://nvie.com/posts/a-successful-git-branching-model/) for a detailed
 explanation). Briefly, there is a *master* branch that always contains the latest tested stable version (tagged with a
 version number). This branch is protected and only the repository owner can push to it. To contribute to the repository,
 please adhere to the following steps:
+
 1. Locally, create a new branch starting at the newest commit of the master branch. You should do all your coding and
    modifications in this feature branch.
-2. Develop in your local feature branch until you reach a state that you like to submit. This may encompass multiple 
+2. Develop in your local feature branch until you reach a state that you like to submit. This may encompass multiple
    local commits.
 3. Use concise and meaningful commit messages that help to track changes.
 4. If the repository gets updated during your development, please merge the new master commit into your feature branch
    and resolve merge conflicts, if any occur.
-5. Make sure that all your changes are reflected in the repositories documentation and modify the documentation if 
+5. Make sure that all your changes are reflected in the repositories documentation and modify the documentation if
    needed.
 6. Do not assign new version numbers. This will be done during the next release of the repository.
 7. Test your code extensively!
@@ -375,10 +383,12 @@ please adhere to the following steps:
 9. Create then a merge request for your pushed branch (= source branch) into the master branch.
 10. Assign the owner of the repository to the merge request.
 11. Your changes will be reviewed by the owner of the repository and the merge will be performed, or you may be asked
-    for additional modifications of your code. 
+    for additional modifications of your code.
 
 ## Components
+
 If you are requesting a merge containing component code, please make sure that the following applies:
+
 - [ ] The commit that is requested to branch is based on the most recent commit on the master branch.
 - [ ] The repository can be cloned from GitLab.
 - [ ] The component runs successfully without any errors using the most recent model version.
@@ -386,14 +396,18 @@ If you are requesting a merge containing component code, please make sure that t
 - [ ] You haven't introduced inputs to the component that are not needed for calculations.
 
 ## Model variant
+
 If you are requesting a merge relating to a model variant, please make sure that the following applies:
+
 - [ ] The commit that is requested to branch is based on the most recent commit on the master branch.
 - [ ] The entire model, including all submodules, can be cloned from GitLab.
 - [ ] The model runs successfully without any errors using the most recent model version.
 - [ ] You haven't reverted any changes made by other contributors unless there is a good reason to do so.
 
 ## Scenario
+
 If you are requesting a merge relating to a scenario, please make sure that the following applies:
+
 - [ ] The commit that is requested to branch is based on the most recent commit on the master branch.
 - [ ] The repository can be cloned from GitLab.
 - [ ] The component runs successfully without any errors using the most recent model version.
@@ -448,23 +462,14 @@ Make sure you use the latest version of the Landscape Model.
 Copy the complete scenario folder unaltered into the `scenario` subdirectory of your model. Reference the scenario
 from the model parameterization. For details how to reference the scenario from the user parameterization, see the 
 `README` of the model.
-""")
-        for source in scenario_info.findall("ExternalSources/ExternalSource"):
-            f.write(f"""
-**This scenario requires an additional resource. Please download the resource from 
-[{inspect.cleandoc(source.text)}]({inspect.cleandoc(source.text)}) 
-and put it into the scenario folder.**""")
-        f.write("""
+
 
 ## Usage
 The scenario adds the following macros to the Landscape Model:
 """)
         for item in scenario_info.findall("Content/Item"):
             f.write(f"* `:{item.attrib['name']}` (version {item.attrib['version']})\n")
-        f.write(f"""
-The scenario covers a time span from `{scenario_info.find("TemporalExtent/FromDate").text}` to 
-`{scenario_info.find("TemporalExtent/ToDate").text}`.
-
+        f.write("""
 ### Roadmap
 The scenario is final and not further developed. It will be, however, updated to reflect new requirements by the 
 Landscape Model core and individual Landscape Model variants.
